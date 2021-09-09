2bc05b47dee59cfbca8613828b662ad2766a78a8
Jathavan Mahendrarajah <73867299+Rickfdalton@users.noreply.github.com>
2021-09-09 07:55:12 +0530

    Delete web_development_project directory

--- web_development_project/script.js
+++
@@ -1,8 +0,0 @@
-const selectBoxes = document.querySelectorAll(".select-box");
-const optionsContainers = document.querySelectorAll(".options-container");
-
-for (let i = 0; i < 3; i++) {
-  selectBoxes[i].addEventListener("click", () => {
-    optionsContainers[i].classList.toggle("active");
-  });
-}
